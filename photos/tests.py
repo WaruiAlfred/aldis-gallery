c8272e6d84203736d53a1fe22867945fcd5a66d1
Alfred Warui <alfredwarui17@gmail.com>
2021-10-08 17:58:01 +0300

    create tests for image location model

--- photos/tests.py
+++ photos/tests.py
@@ -32,3 +32,32 @@ class CategoryTestClass(TestCase): #category test class
     categories = Category.objects.all()
     self.assertTrue(len(categories)==0)
     
+class LocationTestClass(TestCase): #location test class
+  
+  def setUp(self): 
+    '''Function that runs before every test'''
+    self.new_location = Location(location = "Town",date="2021-09-13 10:00:00")
+    
+  def test_instance(self): 
+    '''Function testing that an instance of the object exists'''
+    self.assertTrue(isinstance(self.new_location,Location))
+    
+  def test_save_method(self): 
+    '''Function testing the save method'''
+    self.new_location.save_location()
+    locations = Location.objects.all()
+    self.assertTrue(len(locations)>0)
+    
+  def test_update_method(self): 
+    '''Function testing the update method'''
+    self.new_location.save_location()
+    Location.objects.filter(location = "Town").update(location = "Countryside")
+    update = Location.objects.get(location = "Countryside")
+    self.assertEqual(self.new_location,update)
+    
+  def test_delete_method(self):
+    '''Function testing the delete method''' 
+    self.new_location.save_location()
+    self.new_location.delete_location()
+    locations = Location.objects.all()
+    self.assertTrue(len(locations)==0)
