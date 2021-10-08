c501a2c289281f2acc879f4400bd389e5b81b843
Alfred Warui <alfredwarui17@gmail.com>
2021-10-08 16:12:20 +0300

    add urls for category and location search functionalities

--- photos/urls.py
+++ photos/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls.static import static
 
 #url patterns for the app urls
 urlpatterns = [
-  path('',views.home,name='home')
+  path('',views.home,name='home'),
+  path('search_id/<int:id>',views.search_by_id,name='get_image_by_id'),
+  path('search_category/',views.search_by_category,name='get_image_by_category'),
+  path('search_location/',views.search_by_location,name='get_image_by_location'),
 ]
 
 #configuring uploaded images url
