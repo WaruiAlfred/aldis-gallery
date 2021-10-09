72b8446f94ad4b2c5997c38db9bd3c51fac56de4
Alfred Warui <alfredwarui17@gmail.com>
2021-10-09 19:55:37 +0300

    crud tests for image model

--- photos/tests.py
+++ photos/tests.py
@@ -61,3 +61,53 @@ class LocationTestClass(TestCase): #location test class
     self.new_location.delete_location()
     locations = Location.objects.all()
     self.assertTrue(len(locations)==0)
+    
+class ImageTestClass(TestCase): #image test class
+  
+  def setUp(self): 
+    '''Function that runs before every test'''
+    
+    #creating a new image category and saving it
+    self.new_category = Category(category_name = "Food")
+    self.new_category.save_image_category()
+    
+    #creating a new location and saving it
+    self.new_location = Location(location = "Town",date="2021-09-13 10:00:00")
+    self.new_location.save_location()
+    
+    #creating a new image model instance
+    self.new_image = Image(image_name="Shang Chi",
+                           image_description="It's an epic comic movie.",
+                           category=self.new_category,
+                           location_taken=self.new_location)
+    
+  def tearDown(self):
+    Category.objects.all().delete()
+    Location.objects.all().delete()
+    Image.objects.all().delete()
+    
+  def test_instance(self): 
+    '''Function testing that an instance of the object exists'''
+    self.assertTrue(isinstance(self.new_image,Image))
+    
+  def test_save_method(self): 
+    '''Function testing the save method'''
+    self.new_image.save_image()
+    images = Image.objects.all()
+    self.assertTrue(len(images)>0)
+    
+  def test_update_method(self): 
+    '''Function testing the update method'''
+    self.new_image.save_image()
+    Image.objects.filter(image_name="Shang Chi").update(image_name="Avengers Endgame")
+    update = Image.objects.get(image_name="Avengers Endgame")
+    self.assertEqual(self.new_image,update)
+    
+  def test_delete_method(self):
+    '''Function testing the delete method''' 
+    self.new_image.save_image()
+    self.new_image.delete_image()
+    images = Image.objects.all()
+    self.assertTrue(len(images)==0)
+    
+ 
